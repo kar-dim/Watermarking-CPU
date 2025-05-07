ff1c4353f7a8fbbfe1c3fb5725cec5b2930d4172
Mimis <dim_kar@outlook.com>
2025-05-08 00:04:30 +0300

    inline VideoProcessingContext

--- Watermark_CPU/videoprocessingcontext.hpp
+++ Watermark_CPU/videoprocessingcontext.hpp
@@ -21,5 +21,9 @@ struct VideoProcessingContext
     uint8_t* inputFramePtr;
 
     VideoProcessingContext(AVFormatContext* inputCtx, AVCodecContext* decoderCtx, const int streamIdx,
-        Watermark* watermark, const int h, const int w, const int interval, uint8_t* pixelData);
+        Watermark* watermark, const int h, const int w, const int interval, uint8_t* pixelData) 
+        : inputFormatCtx(inputCtx), inputDecoderCtx(decoderCtx), videoStreamIndex(streamIdx), watermarkObj(watermark),
+        height(h), width(w), watermarkInterval(interval), inputFramePtr(pixelData)
+    {
+    }
 };
